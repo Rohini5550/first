bb61378a96249109c0a1107879614dc47f8f62c0
Rohini5550 <rohi93pro@gmail.com>
2025-04-26 20:18:07 +0530

    to add all

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,6 @@ public class DemoApplication {
 		System.out.println("to check pull and fetch");
 		
 		System.out.println("to check pull and fetch using git");
-		
 		System.out.println("check multiple file add /push");
 	}
 
